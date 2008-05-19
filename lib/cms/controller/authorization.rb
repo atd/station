37f662f9fede77771ad57860db4df2a7be35403c
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-05-19 15:40:10 +0000

    Authorization methods and filters, render 403 errors
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@153 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- lib/cms/controller/authorization.rb
+++ lib/cms/controller/authorization.rb
@@ -38,26 +38,63 @@ module CMS
       def self.included(base) #:nodoc:
         base.send :include, CMS::Controller::Authentication unless base.instance_methods.include?('authenticated?')
 
-        base.class_eval do
-          alias_method_chain :method_missing, :authorization_filters
-        end        
+        authorization_filters_proc = lambda do
+          alias_method :method_missing_without_authorization_methods, :method_missing
+          alias_method :method_missing_without_authorization_filters, :method_missing_with_authorization_methods
+          alias_method :method_missing, :method_missing_with_authorization_filters
+        end
+
+        base.class_eval &authorization_filters_proc
+
+	      authorization_methods_proc = lambda do
+          alias_method_chain :method_missing, :authorization_methods
+       	end
+        
+        base.helper_method :method_missing_with_authorization_methods
+       	base.master_helper_module.module_eval &authorization_methods_proc
+      end
+      
+      # Set HTTP Forbidden (403) response for actions not authorized
+      def not_authorized
+        respond_to do |format|
+          format.html do
+            render :file => "#{RAILS_ROOT}/public/403.html", 
+                   :status => 403
+          end
+          
+          for mime in CMS.mime_types
+            format.send mime.to_sym do
+              render :text => 'Forbidden',
+                     :status => 403
+            end
+          end
+        end
       end
       
       protected
 
       # Hook for CMS Authorization Filters
-      def method_missing_with_authorization_filters(method, *args, &block) #:nodoc:
+      def method_missing_with_authorization_methods(method, *args, &block) #:nodoc:
         if method.to_s =~ /^can__(.*)__(.*)$/
           action = "#{ $1 }_by?"
           object = instance_variable_get("@#{ $2 }")          
           raise Exception.new("Filter #{ method }: can't find variable: @#{ $2 }") unless object
           raise Exception.new("Filter #{ method }: Object #{ object } doesn't respond to action: #{ action }") unless object.respond_to?(action)
           
-          access_denied unless object.send(action, current_agent)       
+          object.send(action, current_agent)       
+        else
+          method_missing_without_authorization_methods(method, *args, &block)
+        end
+      end
+
+      def method_missing_with_authorization_filters(method, *args, &block) #:nodoc:
+        if method.to_s =~ /^(can__.*)__filter$/
+          not_authorized unless send($1)       
         else
           method_missing_without_authorization_filters(method, *args, &block)
         end
-      end     
+      end
+      
     end
   end
 end
