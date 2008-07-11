2322efe63eda00584a340c5b4e670ab0b163f826
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-07-11 13:59:47 +0000

    Rails 2.1, gem dependencies
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@189 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- init.rb
+++ init.rb
@@ -1,3 +1,10 @@
+config.gem "ruby-openid", :lib => 'openid', :version => '>= 2.0.4'
+config.gem "atom-tools", :lib => 'atom/service', :version => '>= 2.0.1'
+config.gem "hpricot", :version => '>= 0.6'
+config.gem "mislav-will_paginate", :lib => 'will_paginate', 
+                                   :version => '>= 2.3.2',
+                                   :source => 'http://gems.github.com/'
+
 unless defined? CMS
   require 'cms'
   CMS.enable
