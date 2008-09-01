8e9ff68a229befd5e685b8524da083e3e1fe8a3b
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-09-01 08:02:11 +0000

    Configurable actions in sortable_list
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@267 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- app/helpers/sortable_helper.rb
+++ app/helpers/sortable_helper.rb
@@ -1,28 +1,30 @@
 module SortableHelper
   # Renders Sortable List table
   #
-  # object_list:: an Array of objects to be listed
-  # object_class:: the class that acts_as_sortable and defines sortable Columns
+  # list:: an Array of objects to be listed
+  # list_class:: the class that acts_as_sortable. It defines sortable Columns and the path to the list queries
   #
   # Available options are:
-  # path:: Array of objects that will be appended to polymorphic_path to build the collection path. Useful for nested resources.
+  # path:: Array of objects that will be appended to polymorphic_path to build the list path. Useful for nested resources.
   # append:: Append string to sortable requests. Example: <tt>"&q=#{ params[:q] }"</tt>
   # table_class:: Class of the html +table+
   # table_id:: ID of the html +table+
+  # actions:: Actions to be performed on object. Defaults to <tt>[ :show, :edit, :delete ]</tt>
   # icon_path:: Path to action icons (not implemented)
-  def sortable_list(object_list, object_class, options = {})
-    object_path = (Array(options[:path]) + Array(object_class.new)).compact
-    options[:table_class] ||= "#{ object_class.to_s.tableize }_list"
-    options[:table_id] ||= "#{ object_class.to_s.tableize }_list"
+  def sortable_list(list, list_class, options = {})
+    list_path = (Array(options[:path]) + Array(list_class.new)).compact
+    options[:table_class] ||= "#{ list_class.to_s.tableize }_list"
+    options[:table_id] ||= "#{ list_class.to_s.tableize }_list"
+    options[:actions] ||= [ :show, :edit, :delete ]
 
     returning "" do |html|
       html << "<table class=\"#{ options[:table_class].to_s }\" id=\"#{ options[:table_id].to_s }\">"
       html << '<tr>'
-      for column in object_class.sortable_columns
+      for column in list_class.sortable_columns
         html << '<th>'
         unless column.no_sort?
-          html << link_to("", "#{ polymorphic_path(object_path) }?order=#{ column.order }&direction=desc#{ options[:append] }", :class => "sortable desc" )
-          html << link_to("", "#{ polymorphic_path(object_path) }?order=#{ column.order }&direction=asc#{ options[:append] }", { :class => "sortable asc" })
+          html << link_to("", "#{ polymorphic_path(list_path) }?order=#{ column.order }&direction=desc#{ options[:append] }", :class => "sortable desc" )
+          html << link_to("", "#{ polymorphic_path(list_path) }?order=#{ column.order }&direction=asc#{ options[:append] }", { :class => "sortable asc" })
         end
         html << "<label>#{ column.name.t }</label>"
         html << "</th>"
@@ -30,15 +32,25 @@ module SortableHelper
       html << '<th class="list_actions">'
       html << '</th>'
       html << '</tr>'
-      for object in object_list
+      for object in list
         html << "<tr class=\"style_#{ cycle('0', '1') }\">"
-        for column in object_class.sortable_columns
+        for column in list_class.sortable_columns
           html << "<td>#{ sanitize column.data(self, object).to_s }</td>"
         end
+
         html << '<td class="list_actions">'
-        html << link_to(image_tag("actions/show.png", :alt => "Show".t, :plugin => 'cmsplugin'), polymorphic_path(object))
-        html << link_to(image_tag("actions/edit.png", :alt => "Edit".t, :plugin => 'cmsplugin'), edit_polymorphic_path(object))
-        html << link_to(image_tag("actions/delete.png", :alt => "Delete".t, :plugin => 'cmsplugin'), polymorphic_path(object), :confirm => 'Are you sure?'.t, :method => :delete)
+        # Show
+        html << link_to(image_tag("actions/show.png", :alt => "Show".t, :plugin => 'cmsplugin'), polymorphic_path(object)) if options[:actions].delete(:show)
+
+        # Delete
+        delete_html = link_to(image_tag("actions/delete.png", :alt => "Delete".t, :plugin => 'cmsplugin'), polymorphic_path(object), :confirm => 'Are you sure?'.t, :method => :delete) if options[:actions].delete(:delete)
+
+        # Rest of actions
+        options[:actions].each do |a|
+          html << link_to(image_tag("actions/#{ a }.png", :alt => a.to_s.humanize.t, :plugin => 'cmsplugin'), send("#{ a }_polymorphic_path", object))
+        end
+
+        html << delete_html
         html << '</td>'
         html << '</tr>'
       end
