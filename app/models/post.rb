a9f40d610d9fbd1eae3cc495963ec957a1a80eaa
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-09-03 16:33:09 +0000

    Solve callbacks problems for Post
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@278 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,7 +19,7 @@ class Post < ActiveRecord::Base
   cattr_reader :collection
   @@collection = :posts
 
-  belongs_to :content,   :polymorphic => true, :dependent => :destroy
+  belongs_to :content,   :polymorphic => true
   belongs_to :container, :polymorphic => true
   belongs_to :agent,     :polymorphic => true
 
