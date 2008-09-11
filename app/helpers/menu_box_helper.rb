b41cba244eb3060a19f4cbea86296aedc60b77d8
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-09-11 14:01:06 +0000

    Modify MenuBoxHelper to allow changing item links
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@297 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- app/helpers/menu_box_helper.rb
+++ app/helpers/menu_box_helper.rb
@@ -1,6 +1,12 @@
 module MenuBoxHelper
-  def menu_box(symbol)
-    <<-"end_html"
+  def menu_box(symbol, &block)
+
+    block ||= lambda { |item|
+      "#{ link_to(icon_image(item, :alt => item.name + ' logo'), item) }<span class=\"box_unit_title\">#{ link_to item.name, item }</span>"
+    }
+
+    returning "" do |html|
+      html << <<-"end_html"
   <div id="#{ symbol }">
     <div id="#{ symbol }_top"></div>
     <div id="#{ symbol }_center">
@@ -14,7 +20,19 @@ module MenuBoxHelper
             <span class="box_title">#{ symbol.to_s.humanize.singularize.t(symbol.to_s.humanize.pluralize, 99) }</span>
             <div class="box_view_wrapper" id="#{ symbol }_wrapper">
               <div class="box_view_list" id="#{ symbol }_list">
-              #{ menu_box_items(symbol) }
+      end_html
+              #{ menu_box_items(symbol, options) }
+      items = instance_variable_get("@#{ symbol }") || Array.new
+
+      items.inject html do |html, item|
+        html << "<div class=\"box_unit\">"
+        html << yield(item)
+#        html << send(link_method, image_tag(icon_image(item), :alt => item.name + " logo"), 
+#        html << "<span class=\"box_unit_title\">#{ link_to item.name, item }</span>"
+        html << "</div>"
+      end
+
+      html << <<-"end_html"
               </div>
             </div>
           </div>
@@ -26,18 +44,7 @@ module MenuBoxHelper
     </div>
     <div id="#{ symbol }_bottom"></div>
   </div>
-    end_html
-  end
-
-  def menu_box_items(symbol)
-    items = instance_variable_get("@#{ symbol }") || Array.new
-    returning "" do |html|
-      items.inject html do |html, item|
-        html << "<div class=\"box_unit\">"
-        html << link_to(image_tag(icon_image(item), :alt => item.name + " logo"), item)
-        html << "<span class=\"box_unit_title\">#{ link_to item.name, item }</span>"
-        html << "</div>"
-      end
+      end_html
     end
   end
 end
