1ba79253b0e1e56ee3eb5e997a60f6ce6db7aba7
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-09-17 11:22:24 +0000

    Add ActionView::Helpers::AtomFeedHelper#atom_entry
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@301 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- lib/cms/core_ext.rb
+++ lib/cms/core_ext.rb
@@ -17,6 +17,41 @@ unless NilClass.instance_methods.include? 'to_ppath'
   end
 end
 
+unless ActionView::Helpers::AtomFeedHelper.respond_to?(:atom_entry)
+  ActionView::Helpers::AtomFeedHelper.module_eval do
+    def atom_entry(record, options = {}, &block)
+      if options[:schema_date]
+        options[:schema_date] = options[:schema_date].strftime("%Y-%m-%d") if options[:schema_date].respond_to?(:strftime)
+      else
+        options[:schema_date] = "2005" # The Atom spec copyright date
+      end
+
+      xml = options[:xml] || eval("xml", block.binding)
+      xml.instruct!
+
+      entry_opts = {"xml:lang" => options[:language] || "en-US", "xmlns" => 'http://www.w3.org/2005/Atom'}
+      entry_opts.merge!(options).reject!{|k,v| !k.to_s.match(/^xml/)}
+
+      xml.entry(entry_opts) do
+        xml.id("tag:#{request.host},#{options[:schema_date]}:#{record.class}/#{record.id}")
+
+        if options[:published] || (record.respond_to?(:created_at) && record.created_at)
+          xml.published((options[:published] || record.created_at).xmlschema)
+        end
+
+        if options[:updated] || (record.respond_to?(:updated_at) && record.updated_at)
+          xml.updated((options[:updated] || record.updated_at).xmlschema)
+        end
+
+        xml.link(:rel => 'alternate', :type => 'text/html', :href => options[:root_url] || polymorphic_url(record))
+        xml.link(:rel => 'self', :type => 'application/atom+xml', :href => options[:url] || request.url)
+
+        yield ActionView::Helpers::AtomFeedHelper::AtomFeedBuilder.new(xml, self, options)
+      end
+    end
+  end
+end
+
 class ActionController::Resources::Resource
   attr_reader :module_namespace
 
