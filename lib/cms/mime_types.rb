9f1cf7365a41f3e287a2186c58a9a2e10ea0ecc8
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-09-29 18:49:24 +0000

    Remove loading of other MimeTypes (because of param_parsers)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@321 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- lib/cms/mime_types.rb
+++ lib/cms/mime_types.rb
@@ -8,10 +8,5 @@ module CMS
     Mime::Type.register "application/atomsvc+xml", :atomsvc
     Mime::Type.register "application/atomcat+xml", :atomcat
     Mime::Type.register "application/xrds+xml",    :xrds
-
-    # Include Application MimeTypes
-    # FIXME: this is necessary for CMS.param_parsers load
-    # but introduces warnings when loaded by Rails Initializers
-    load RAILS_ROOT + '/config/initializers/mime_types.rb'
   end
 end
