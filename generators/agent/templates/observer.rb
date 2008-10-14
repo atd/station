194d334fdc0ca2997ce828d14c89343f56b73422
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-10-14 09:25:37 +0000

    Fix agent observer template
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@341 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- generators/agent/templates/observer.rb
+++ generators/agent/templates/observer.rb
@@ -4,10 +4,10 @@ class <%= class_name %>Observer < ActiveRecord::Observer
   end
 
   def after_save(<%= file_name %>)
-  <% if options[:include_activation] -%>
-    <%= class_name %>Mailer.deliver_activation(<%= file_name %>) if <%= file_name %>.pending?
-    <%= class_name %>Mailer.deliver_forgot_password(<%= file_name %>) if <%= file_name %>.recently_forgot_password?
-    <%= class_name %>Mailer.deliver_reset_password(<%= file_name %>) if <%= file_name %>.recently_reset_password?
-  <% end -%>
+    if <%= file_name %>.class.agent_options[:authentication].include?(:login_and_password) && <%= file_name %>.class.agent_options[:activation]
+      <%= class_name %>Mailer.deliver_activation(<%= file_name %>) if <%= file_name %>.pending?
+      <%= class_name %>Mailer.deliver_forgot_password(<%= file_name %>) if <%= file_name %>.recently_forgot_password?
+      <%= class_name %>Mailer.deliver_reset_password(<%= file_name %>) if <%= file_name %>.recently_reset_password?
+    end
   end
 end
