9ba75ff1d09f6686bc696e5602415d0a630d5bc1
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-11-19 16:55:10 +0000

    Change Role#name uniqueness scope
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@394 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,7 +6,7 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :permissions
 
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :stage_type
 
   acts_as_sortable :columns => [ :name, :stage_type ]
 
