cbb0d623d486a5ae74b3b82831542a40c0719992
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-12-01 16:43:31 +0000

    Fix AuthenticationTestHelper#find_agent_fixture
    git-svn-id: svn+ssh://rubyforge.org/var/svn/cmsplugin/trunk@421 36a375c3-9404-4128-a23d-939a5b6d6d8e

--- lib/cms/authentication_test_helper.rb
+++ lib/cms/authentication_test_helper.rb
@@ -26,7 +26,7 @@ module CMS
     def find_agent_fixture(agent)
       return nil unless agent
 
-      ( CMS::ActiveRecord::Agent.symbols - [ :singular_agents ] ) do |agent_klass|
+      ( CMS::ActiveRecord::Agent.symbols - [ :singular_agents ] ).each do |agent_klass|
         agent_fixture = send(agent_klass, agent)
         return agent_fixture if agent_fixture
       end
