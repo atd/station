0f9d7c7e0b599ae3d843537073c629bce37e1ebc
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-04-03 10:30:30 +0200

    Don't show tags_list title when there aren't any tags

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,9 +2,11 @@ module TagsHelper
   def tag_list(taggable)
     returning "" do |html|
       html << "<div id=\"tag_list_#{ dom_id(taggable) }\" class=\"tag_list\">"
-      html << "<strong>#{ t('tag.other') }</strong>: "
-      html << taggable.tags.map { |t| 
-        link_to(t.name, tag_path(t), :rel => "tag") }.join(", ")
+      if taggable.tags.any?
+        html << "<strong>#{ t('tag.other') }</strong>: "
+        html << taggable.tags.map { |t| 
+          link_to(t.name, tag_path(t), :rel => "tag") }.join(", ")
+      end
       html << '</div>'
     end
   end
