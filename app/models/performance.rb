76491b1fb383ee7974cee1dbbc1734ddb460afa9
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-08-31 11:34:03 +0200

    Performance#avoid_first_role_downgrade

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -25,6 +25,8 @@ class Performance < ActiveRecord::Base
   validates_uniqueness_of :agent_id, :scope => [ :agent_type, :stage_id, :stage_type ]
   validates_uniqueness_of :agent_type, :scope => [ :agent_id, :stage_id, :stage_type ]
 
+  validate_on_update :avoid_uniq_first_role_downgrade
+
   def to_acl
     raise "Performance #{ id } hasn't any Role!" if role.blank?
 
@@ -34,4 +36,15 @@ class Performance < ActiveRecord::Base
       acl << [ agent, p]
     end
   end
+
+  def avoid_uniq_first_role_downgrade
+    if role_id_changed? &&
+       role_id_was == stage.class.roles.sort.last.id &&
+       Performance.find_all_by_stage_id_and_stage_type_and_role_id(stage.id, stage.class.base_class.to_s, role_id_was).count < 2
+
+      errors.add(:role_id, I18n.t('performance.errors.stage_should_not_run_out_of_performances_with_first_role',
+                             :role => stage.class.roles.sort.last.name))
+    end
+  end
+
 end
