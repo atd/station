936ebd31fe87574c783091d3b2fc101bcd341593
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-11-18 12:23:28 +0100

    Add current_container support in TagsController#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,7 +6,7 @@ class TagsController < ApplicationController
   private
 
   def tag
-    @tag ||= Tag.find_by_name(params[:id])
+    @tag ||= (current_container.try(:tags) || Tag).find_by_name(params[:id])
     raise ActiveRecord::RecordNotFound, "Tag not found" unless @tag
 
     @tag
