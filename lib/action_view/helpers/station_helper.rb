dfdcb5068cda07f8b9a904506de701169a43630f
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-02-18 12:25:21 +0100

    Clean StationHelper#title

--- lib/action_view/helpers/station_helper.rb
+++ lib/action_view/helpers/station_helper.rb
@@ -4,23 +4,22 @@ module ActionView #:nodoc:
     module StationHelper
       # Get title in this order:
       # 1. string argument 
-      # 2. @title instance variable
-      # 3. Title based on variables set by the Controller
-      # 4. <tt>controller.controller_name</tt> - <tt>current_site.name</tt>
+      # 2. Title based on variables set by the Controller
+      # 3. <tt>controller.controller_name</tt> - <tt>current_site.name</tt>
       #
       # Options:
       # <tt>append_site_name</tt>:: Append the Site name to the title, ie, "Title - Example Site". Defaults to <tt>false</tt>
       #
-      def title(new_title = "", options = {})
-        title = if new_title.present?
-                  new_title
-                elsif @title
-                  @title
-                elsif @resources
+      def title(*args)
+        options = args.extract_options!
+
+        title = if args.first.present?
+                  args.first
+                elsif @resources # Use instance. Don't call the method and query database
                   path_container ?
                     t(:other_in_container, :scope => controller.controller_name.singularize, :container => path_container.name) :
                     t(:other, :scope => controller.controller_name.singularize)
-                elsif @resource
+                elsif @resource # Use instance. Don't call the method and query database
                   if @resource.new_record?
                     t(:new, :scope => @resource.class.to_s.underscore)
                   elsif controller.action_name == 'edit' || @resource.errors.any?
